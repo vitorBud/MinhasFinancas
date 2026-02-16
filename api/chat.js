a842b7b09afc43192cdd3227605768c5cfb01594
vitor Gomes <vitor.probem@gmail.com>
2026-02-15 22:44:50 -0300

    api working2

--- api/chat.js
+++ api/chat.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
     }
 
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=" + apiKey,
+      "https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=" + apiKey,
       {
         method: "POST",
         headers: {
